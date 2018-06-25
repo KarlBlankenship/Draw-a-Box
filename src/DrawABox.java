c052a9b9f230916244330fd655132b7385883afe
Karl Blankenship <Karleb777@gmail.com>
2018-06-25 10:09:11 -0500

    Added sample code for dialog boxes.

--- src/DrawABox.java
+++ src/DrawABox.java
@@ -14,6 +14,14 @@
 //																				//
 //////////////////////////////////////////////////////////////////////////////////
 
+import javax.swing.JOptionPane;		// Import for creation of dialog boxes.
+
+
+/*
+	The DrawABox class will create instances of the Box class and will diplay
+	a visual representation of the box in an output window.
+*/
+
 public class DrawABox {
 
 	public static void main(String[] args) {
@@ -27,20 +35,68 @@ public class DrawABox {
 		// Open a display window
 		// Generate lines connecting the coordinates
 		
-		// Test the Box class.
-		// Create a new instance using default constructor.
-		Box shippingBox = new Box();
-		// Create a new instance using the overload constructor.
-		Box bigBox = new Box(12.0, 8.0, 4.0);
-		// Get the length of the shippingBox instance.
-		System.out.println("length of shippingBox = " + shippingBox.getLength());
-		// Get the width of the shippingBox instance.
-		System.out.println("width of shippingBox = " + shippingBox.getWidth());
-		// Get the height of the shippingBox instance.
-		System.out.println("height of shippingBox = " + shippingBox.getHeight());
-		// Use toString to verify values of bigBox.
-		System.out.print("toString bigBox:" + bigBox.toString());
-		// Box Class is working.
+		/*
+			// Test the Box class.
+			// Create a new instance using default constructor.
+			Box shippingBox = new Box();
+			// Create a new instance using the overload constructor.
+			Box bigBox = new Box(12.0, 8.0, 4.0);
+			// Get the length of the shippingBox instance.
+			System.out.println("length of shippingBox = " + shippingBox.getLength());
+			// Get the width of the shippingBox instance.
+			System.out.println("width of shippingBox = " + shippingBox.getWidth());
+			// Get the height of the shippingBox instance.
+			System.out.println("height of shippingBox = " + shippingBox.getHeight());
+			// Use toString to verify values of bigBox.
+			System.out.print("toString bigBox:" + bigBox.toString());
+			// Box Class is working.
+		*/
+		
+		// Creation of dialog box code to ask for inputs.
+		// Test display a message.
+		JOptionPane.showMessageDialog(null,  "Hello Box");
+		// Test entering a dimension.
+		String dimension;
+		dimension = JOptionPane.showInputDialog("Enter a dimension.");
+		// Test displaying entered dimension.
+		JOptionPane.showMessageDialog(null,  dimension);
+		// Convert dimension to a double.
+		double doubleDimension = 0.0;			// Initialize input as a double.
+		doubleDimension = Double.parseDouble(dimension);
+		System.out.println("Dimension as a double = " + doubleDimension);
+		// Test that it is a double.
+		System.out.println("Dimension x 3.5 = " + doubleDimension * 3.5);
+		// JOPtionPane requires a system exit.
+		System.exit(0);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 
 	}
