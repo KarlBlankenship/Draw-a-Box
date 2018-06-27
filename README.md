af7e775fc398a2c0fd75a4f729a1fbe71e9d3b3a
Karl Blankenship <Karleb777@gmail.com>
2018-06-27 11:18:48 -0500

    Cleaned up comments

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 #Draw a box
 This is a practice file to gain experience creating classes and 
 displaying graphics to an output window using Java.
+The initial plan is to draw a user defined rectangle on the screen.
+Additional plans will be to create a 3D representation of a box on
+the screen.
 Karl Blankenship
--- src/DrawABox.java
+++ src/DrawABox.java
@@ -4,9 +4,9 @@
 //	and draw a box fitting those dimensions in a pop-up window.					//
 //																				//
 //	Development notes... I will probably start out testing with drawing a		//
-//	2-D (length and width) rectangle before trying to draw a 3D 				//
-//	representation of the box. Also, this exercise can be expanded on to		//
-//	include shading, features, rotation, etc.									//
+//	2-D (width and height) rectangle before trying to draw a 3D 				//
+//	representation of the box. Also, this exercise can continue to be 			//
+//  expanded to	include shading, features, rotation, etc.						//
 //																				//
 //	Karl Blankenship															//
 //																				//
@@ -23,7 +23,10 @@ import javax.swing.JFrame;			//
 
 /*
 	The DrawABox class will create instances of the Box class and will display
-	a visual representation of the box in an output window.
+	a visual representation of the box in an output window. Box class is currently
+	not required to display a single rectangle on the screen. However, as my
+	course progresses I plan to utilize the box class to create multiple box
+	objects to display.
 */
 
 public class DrawABox extends Canvas
@@ -32,80 +35,32 @@ public class DrawABox extends Canvas
 	private static final long serialVersionUID = 1L;
 	
 	public static void main(String[] args) {
-		
-		// Rough plan...
-		// Create a box object (need a box class)
-		// Display a message box asking for length
-		// Display a message box asking for width
-		// Display a message box asking for height
-		// Calculate the 3-D perspective coordinates
-		// Open a display window
-		// Generate lines connecting the coordinates
-		
-		
-		/*
-			// Test the Box class.
-			// Create a new instance using default constructor.
-			Box shippingBox = new Box();
-			// Create a new instance using the overload constructor.
-			Box bigBox = new Box(12.0, 200.0, 4.0);
-			// Get the length of the shippingBox instance.
-			System.out.println("length of shippingBox = " + shippingBox.getLength());
-			// Get the width of the shippingBox instance.
-			System.out.println("width of shippingBox = " + shippingBox.getWidth());
-			// Get the height of the shippingBox instance.
-			System.out.println("height of shippingBox = " + shippingBox.getHeight());
-			// Use toString to verify values of bigBox.
-			System.out.print("toString bigBox:" + bigBox.toString());
-			// Box Class is working.
-		*/
-			
-			
-		/*
-			// Creation of dialog box code to ask for inputs.
-			// Test display a message.
-			JOptionPane.showMessageDialog(null, "Hello Box");
-			// Test entering a dimension.
-			String dimension;
-			dimension = JOptionPane.showInputDialog("Enter a dimension.");
-			// Test displaying entered dimension.
-			JOptionPane.showMessageDialog(null,  dimension);
-			// Convert dimension to a double.
-			double doubleDimension = 0.0;			// Initialize input as a double.
-			doubleDimension = Double.parseDouble(dimension);
-			System.out.println("Dimension as a double = " + doubleDimension);
-			// Test that it is a double.
-			System.out.println("Dimension x 3.5 = " + doubleDimension * 3.5);
-			// JOPtionPane requires a system exit.
-			System.exit(0);
-			// Dialog boxes are working.
-		*/
-		
-		 // Create a simple rectangle
+				
+		// Create the drawing space which calls the paint class
 			JFrame frame = new JFrame("Rectangle"); 	// Create a new window frame.
 			Canvas canvas = new DrawABox(); 			// Create a new canvas object
 			canvas.setSize(500, 500); 					// Set canvas size.
 			frame.add(canvas); 							// Add the canvas to the frame.
 			frame.pack(); 								// Fit the canvas to the frame.
 			frame.setVisible(true);						// Make frame visible.
-		}
+	}
 
 	
 	/* 
-		The paint method will run when the canvas updates.(non-Javadoc)
-	 	@see java.awt.Canvas#paint(java.awt.Graphics)
-	 	
+		The paint method will run when the canvas updates.
 	*/
-	
-	// How do I get my user entered dimensions into this?!?!
-	// Just a few chapters away in text.
+		
 	public void paint(Graphics g)
 	{ 
-		String dimHeight;
-		int width = Integer.parseInt(JOptionPane.showInputDialog("Enter the box width less than 300."));
-		dimHeight = JOptionPane.showInputDialog("Enter the box height less than 300.");
-		int height = Integer.parseInt(dimHeight);
+		// Prompt user to enter a width (need to add dimension check)
+		int width = Integer.parseInt(JOptionPane.showInputDialog("Enter the box " +
+															"width less than 300."));
+		// Prompt user to enter a height (need to add dimension check)
+		int height = Integer.parseInt(JOptionPane.showInputDialog("Enter the box " +
+															"height less than 300."));
 		g.drawRect(100, 100, width, height); 
+		// This works but easily bypasses the Box class meaning only a single box can 
+		// exist at a time.
 	} 
 
 }
